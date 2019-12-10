7a48e6d91d85d40a990c0adadc01aa001f049850
Andrew Cassidy <andee@bytz.co.uk>
2019-12-10 12:39:16 +0000

    day 4 complete

--- day4.py
+++ day4.py
@@ -2,39 +2,38 @@
 
 r = [171309, 643603]
 
+
 def incr_only(num):
-  prev = 0
-  for i in [int(i) for i in str(num)]:
-    if i < prev:
-      return False
-    prev = i
-  return True
+    prev = 0
+    for i in [int(i) for i in str(num)]:
+        if i < prev:
+            return False
+        prev = i
+    return True
+
 
 def contains_double(num):
-  s = str(num)
-  for patt in [str(i) for i in range(11, 121, 11)]:
-    if patt in s:
-      return True
-  return False
+    s = str(num)
+    for patt in [str(i) for i in range(11, 121, 11)]:
+        if patt in s:
+            return True
+    return False
 
-def contains_quad(num):
-  s = str(num)
-  for patt in [str(i) for i in range(1111, 11110, 1111)]:
-    if patt in s:
-      return True
-  return False
 
-def not_triple(num):
-  s = str(num)
-  for patt in [str(i) for i in range(111, 1110, 111)]:
-    if patt in s:
-      return False
-  return True
+def contains_double_exact(num):
+    s = str(num)
+    for x in range(0, 10):
+        dbl = str(x * 11)
+        trp = str(x * 111)
+        if dbl in s and not trp in s:
+            return True
+    return False
 
-l = []  
-for i in range(r[0], r[1]):
-  if incr_only(i) and (contains_quad(i) or (contains_double(i) and not_triple(i))):
-    l.append(i)
 
-print(l)
+# Part 1
+l = [x for x in range(*r) if incr_only(x) and contains_double(x)]
+print(len(l))
+
+# Part 2
+l = [x for x in range(*r) if incr_only(x) and contains_double_exact(x)]
 print(len(l))
