f2f0fc593bc493b3bf8381d605995ae621568fa9
Andrew Cassidy <andee@bytz.co.uk>
2019-12-11 10:08:13 +0000

    Day 3 complete

--- day3.py
+++ day3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import itertools
+
 
 with open('day3.txt', 'r') as f:
     initial = f.readlines()
@@ -11,20 +11,31 @@ for code in initial:
         dir = instr[0]
         mag = int(instr[1:])
         if not line:
-            prev = (0, 0)
+            prev = (0, 0, 0)
         else:
             prev = line[-1][1]
         if dir == "U":
-            new = (prev[0], prev[1] + mag)
+            new = (prev[0], prev[1] + mag, prev[2] + mag)
         elif dir == "D":
-            new = (prev[0], prev[1] - mag)
+            new = (prev[0], prev[1] - mag, prev[2] + mag)
         elif dir == "R":
-            new = (prev[0] + mag, prev[1])
+            new = (prev[0] + mag, prev[1], prev[2] + mag)
         else:
-            new = (prev[0] - mag, prev[1])
+            new = (prev[0] - mag, prev[1], prev[2] + mag)
         line.append((prev, new))
     lines.append(line)
 
-for x, y in itertools.permutations(*lines):
-    # check if both lines
-    pass
+intersections = []
+for x in lines[0]:
+    for y in lines[1]:
+        # Are lines the same orientation?
+        if (x[0][0] == x[1][0] and y[0][0] == y[0][1]) or (x[0][1] == x[1][1] and y[0][1] == y[1][1]):
+            continue
+        xs, ys = sorted(x), sorted(y)
+        if (xs[0][0] <= ys[0][0] <= xs[1][0]) \
+                and (ys[0][1] <= xs[0][1] <= ys[1][1]):
+            dist = x[0][2] + abs(x[0][0] - y[0][0]) + y[0][2] + abs(y[0][1] - x[0][1])
+            intersections.append((ys[0][0], xs[0][1], dist))
+
+print("Part 1:", sorted([abs(x) + abs(y) for x, y, z in intersections])[0])
+print("Part 2:", sorted([z for x, y, z in intersections])[0])
